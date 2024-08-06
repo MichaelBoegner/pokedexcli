08e41dbd618b75b256fa0b64ac8b3610f6c1d819
Charles Michael Boegner <mboegner@charles-boegner.tail16562.ts.net>
2024-08-06 16:19:04 -0400

    basic CLI input loop functional with exit command

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+
+		fmt.Print("Enter something: ")
+		input, err := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			return
+		}
+		fmt.Printf("\nInput: %v", input)
+		if input == "exit()" {
+			break
+		}
+		fmt.Println("You entered:", input)
+	}
 }
