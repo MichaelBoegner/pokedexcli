21163f428ed6e79c414d97c0bd328006e22f1f8b
Charles Michael Boegner <mboegner@charles-boegner.tail16562.ts.net>
2024-08-08 16:06:02 -0400

    returing first 20 locations using newly implemented map command

--- commands.go
+++ commands.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
 )
 
 type cliCommand struct {
 	Name        string
 	Description string
 	Callback    func() error
+}
+
+type ResponseBody struct {
+	Locations []Location `json:"results"`
+}
+
+type Location struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
 }
 
 func (c cliCommand) Commands() map[string]cliCommand {
@@ -22,6 +34,11 @@ func (c cliCommand) Commands() map[string]cliCommand {
 			Name:        "exit",
 			Description: "Exit the Pokedex",
 			Callback:    commandExit,
+		},
+		"map": {
+			Name:        "map",
+			Description: "Displays the names of the last 20 location areas in the Pokemon world. Each subsequent call displays the next 20 locations.",
+			Callback:    commandMap,
 		},
 	}
 }
@@ -37,4 +54,28 @@ func commandHelp() error {
 
 func commandExit() error {
 	return errors.New("Exiting")
+}
+
+func commandMap() error {
+	var unmarshaledBody ResponseBody
+
+	response, err := http.Get("https://pokeapi.co/api/v2/location")
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close() // Ensure the body is closed after reading
+
+	// Read the response body
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+	json.Unmarshal(body, &unmarshaledBody)
+
+	// Print the response body
+	for _, location := range unmarshaledBody.Locations {
+		fmt.Println(location.Name)
+
+	}
+	return nil
 }
